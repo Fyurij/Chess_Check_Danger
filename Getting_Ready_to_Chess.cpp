6f45164674ba3808ef48d5e1f5ea1ad07067d5dd
Jurij <fyurij.zin@gmail.com>
2023-10-31 16:47:39 +0300

    Add classes, reading from file

--- Getting_Ready_to_Chess.cpp
+++ Getting_Ready_to_Chess.cpp
@@ -1,20 +1,208 @@
-﻿// Getting_Ready_to_Chess.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <exception>
+#include <map>
 
-#include <iostream>
+class Figure
+{
+public:
+    virtual int GetX() = 0;
+    virtual int GetY() = 0;
+    virtual std::string GetName() = 0;
+    virtual bool Danger(int i, int j) = 0;
+};
+
+class King : public Figure
+{
+private:
+    int x;
+    int y;
+    std::string name;
+public:
+    King(int x_, int y_, std::string name_)
+        :x(x_)
+        , y(y_)
+        , name(name_)
+    {}
+    int GetX() override
+    {
+        return x;
+    }
+    int GetY() override
+    {
+        return y;
+    }
+    bool Danger(int i, int j) override
+    {
+        return true;
+    }
+};
+
+class Queen : public Figure
+{
+private:
+    int x;
+    int y;
+    std::string name;
+public:
+    Queen(int x_, int y_, std::string name_)
+        :x(x_)
+        , y(y_)
+        , name(name_)
+    {}
+    int GetX() override
+    {
+        return x;
+    }
+    int GetY() override
+    {
+        return y;
+    }
+    bool Danger(int i, int j) override
+    {
+        return true;
+    }
+};
+
+class Rook : public Figure
+{
+private:
+    int x;
+    int y;
+    std::string name;
+public:
+    Rook(int x_, int y_, std::string name_)
+        :x(x_)
+        , y(y_)
+        , name(name_)
+    {}
+    int GetX() override
+    {
+        return x;
+    }
+    int GetY() override
+    {
+        return y;
+    }
+    bool Danger(int i, int j) override
+    {
+        return true;
+    }
+};
+
+class Bishop : public Figure
+{
+private:
+    int x;
+    int y;
+    std::string name;
+public:
+    Bishop(int x_, int y_, std::string name_)
+        :x(x_)
+        , y(y_)
+        , name(name_)
+    {}
+    int GetX() override
+    {
+        return x;
+    }
+    int GetY() override
+    {
+        return y;
+    }
+    bool Danger(int i, int j) override
+    {
+        return true;
+    }
+};
+
+class Knight : public Figure
+{
+private:
+    int x;
+    int y;
+    std::string name;
+public:
+    Knight(int x_, int y_, std::string name_)
+        :x(x_)
+        , y(y_)
+        , name(name_)
+    {}
+    int GetX() override
+    {
+        return x;
+    }
+    int GetY() override
+    {
+        return y;
+    }
+    bool Danger(int i, int j) override
+    {
+        return true;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::ifstream koordinates("Koordinates.txt");
+    if (!koordinates.is_open())
+    {
+        std::cout << "File is not opened" << std::endl;
+        return 0;
+    }
+    std::string figure;
+    int x;
+    int y;
+    std::vector<Figure*> figure_array;
+    while (!koordinates.eof())
+    {
+        koordinates >> figure >> x >> y;
+        
+        if (figure == "king")
+        {
+            Figure* character = new King(x, y, figure);
+            figure_array.push_back(character);
+        }
+        else if (figure == "queen")
+        {
+            Figure* character = new Queen(x, y, figure);
+            figure_array.push_back(character);
+        }
+        else if (figure == "rook")
+        {
+            Figure* character = new Rook(x, y, figure);
+            figure_array.push_back(character);
+        }
+        else if (figure == "bishop")
+        {
+            Figure* character = new Bishop(x, y, figure);
+            figure_array.push_back(character);
+        }
+        else if (figure == "knight")
+        {
+            Figure* character = new Knight(x, y, figure);
+            figure_array.push_back(character);
+        }
+        else
+        {
+            std::cout << "Invalid argument" << std::endl;
+        }
+        std::cout << figure << " " << x << " " << y << std::endl;
+    }
+    for (int i = 0; i < figure_array.size(); i++)
+    {
+        for (int j = 0; j < figure_array.size(); j++)
+        {
+            if (i != j)
+            {
+                bool check = figure_array[i]->Danger(figure_array[j]->GetX(), figure_array[j]->GetY());
+                if (check)
+                {
+                    std::cout << figure_array[i].
+                }
+            }
+        }
+    }
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
